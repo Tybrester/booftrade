44514fb8ec7d5edf3b9ba349af49ce8e073d3a1d
Tybrester <tybrester@gmail.com>
2026-05-03 17:45:49 -0600

    Cleanup: Remove dead code - test functions, unused vars, one-off SQL files (~400 lines removed)

--- cron_setup.sql
+++
@@ -1,5 +0,0 @@
-SELECT cron.schedule(
-  'auto-bot-hourly',
-  '0 * * * *',
-  $$SELECT net.http_post(url:='https://isanhutzyctcjygjhzbn.supabase.co/functions/v1/auto-bot',headers:='{"Content-Type":"application/json"}'::jsonb,body:='{}'::jsonb)$$
-);
